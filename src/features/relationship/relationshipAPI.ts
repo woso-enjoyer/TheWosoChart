56e153935515aa513cab0fe6e5a3b6ac26a059c3
woso-enjoyer <181172168+woso-enjoyer@users.noreply.github.com>
2026-06-27 23:47:31 +0000

    Nicki Hernandez is engaged to Nat Cruz

--- src/features/relationship/relationshipAPI.ts
+++ src/features/relationship/relationshipAPI.ts
@@ -10,7 +10,7 @@ export const fetchRelationships = async (): Promise<Relationship[]> => {
   return sanitizeRelationshipData(relationships)
 }
 
-const labelColorMap: Record<string, string> = { "Gf": "6aa84f", "Rumor": "ab57ff", "Ex": "ff0000", "Wife": "9cc2e5"}
+const labelColorMap: Record<string, string> = { "Gf": "6aa84f", "Rumor": "ab57ff", "Ex": "ff0000", "Wife": "4C60F0", "Fiancée": "9cc2e5"}
 
 export const sanitizeRelationshipData = (data: Relationship[]): Relationship[] => {
     data.forEach((element, index) => {
